1b247822d7be6e57f9970d45f691efd2a21dfcfb
Cleverson <clruas@gmail.com>
2022-10-06 00:23:29 -0300

    Teste de upload e listagem de imagens locais

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useState } from 'react'
 import './app.sass'
 
 function App() {
+	const [files, setFiles] = useState([])
+	const viewPhotos = e => {
+		console.log(files)
+	}
+	const getFiles = e => {
+		const fileList = Array.from(e.target.files)
+		setFiles([])
+		fileList.forEach(file => {
+			const reader = new FileReader()
+			reader.onloadend = e => setFiles(state => [...state, { name: file.name, source: e.target.result }])
+			reader.readAsDataURL(file)
+		})
+	}
 	return <div className="app">
-		PhotoPrint 1.0
+		<input type="file" multiple accept='image/*' onChange={getFiles} />
+		<div>
+			<div>Meus arquivos</div>
+			{files.map(file =><div>
+				<div>{file.name}</div>
+				<img src={file.source} width="120" height="100"/>
+			</div>)}
+		</div>
+		<button onClick={viewPhotos}>Imprimir</button>
 	</div>
 }
 
